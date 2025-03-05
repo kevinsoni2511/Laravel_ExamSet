e30602ee4b5f1666330880951a936b0a02760713
KEVIN SONI <168913187+kevinsoni2511@users.noreply.github.com>
2025-03-05 22:21:18 +0530

    Update orderplaced.blade.php

--- SET-1/resources/views/orderplaced.blade.php
+++ SET-1/resources/views/orderplaced.blade.php
@@ -19,15 +19,4 @@
     </table>
 </div>
 
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        background-color: #f8f9fa;
-        margin: 0;
-        padding: 0;
-    }
-
-    
-</style>
-
 @endsection
